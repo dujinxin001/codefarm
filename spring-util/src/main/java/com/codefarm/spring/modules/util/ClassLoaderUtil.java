2870a27c2b1374286bc11d6c36dd2dd7aa684c7b
zhangjian <zhangjian@coderun99.com>
2018-01-29 23:47:14 +0800

    update jackson version

--- spring-util/src/main/java/com/codefarm/spring/modules/util/ClassLoaderUtil.java
+++ spring-util/src/main/java/com/codefarm/spring/modules/util/ClassLoaderUtil.java
@@ -7,36 +7,28 @@ import java.io.InputStream;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternUtils;
 
-public class ClassLoaderUtil
-{
-    public static InputStream getResourceAsStream(String configFile) throws IOException
-    {
-        //        InputStream configStream = ClassLoaderUtil.class.getClassLoader()
-        //                .getParent()
-        //                .getResourceAsStream(configFile);
-        //        if (configStream == null)
-        //            configStream = ClassLoaderUtil.class.getClassLoader()
-        //                    .getResourceAsStream(configFile);
-        //        if (configStream == null)
-        //            configStream = Thread.currentThread()
-        //                    .getContextClassLoader()
-        //                    .getResourceAsStream(configFile);
-        //        configStream = getResource(ClassLoaderUtil.class.getClassLoader(),
-        //                configFile);
-        
-        InputStream configStream = ResourcePatternUtils
-                .getResourcePatternResolver(
-                        new PathMatchingResourcePatternResolver())
-                .getResource(configFile)
-                .getInputStream();
-        if (configStream == null)
-            throw new FileNotFoundException(
-                    "Cannot find "
-                            + ClassLoaderUtil.class.getClassLoader()
-                                    .getResource(configFile)
-                                    .toExternalForm()
-                            + " !!!");
-        return configStream;
-    }
-    
+public class ClassLoaderUtil {
+	public static InputStream getResourceAsStream(String configFile) throws IOException {
+		// InputStream configStream = ClassLoaderUtil.class.getClassLoader()
+		// .getParent()
+		// .getResourceAsStream(configFile);
+		// if (configStream == null)
+		// configStream = ClassLoaderUtil.class.getClassLoader()
+		// .getResourceAsStream(configFile);
+		// if (configStream == null)
+		// configStream = Thread.currentThread()
+		// .getContextClassLoader()
+		// .getResourceAsStream(configFile);
+		// configStream = getResource(ClassLoaderUtil.class.getClassLoader(),
+		// configFile);
+
+		InputStream configStream = ResourcePatternUtils
+				.getResourcePatternResolver(new PathMatchingResourcePatternResolver()).getResource(configFile)
+				.getInputStream();
+		if (configStream == null)
+			throw new FileNotFoundException("Cannot find "
+					+ ClassLoaderUtil.class.getClassLoader().getResource(configFile).toExternalForm() + " !!!");
+		return configStream;
+	}
+
 }
